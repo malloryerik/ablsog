1725bb0896be5b35c289ee229873687158d72c95
mallory-erik <malloryerik@gmail.com>
2019-02-14 20:45:13 +0900

    minor schema

--- lib/abslog_web/schema.ex
+++ lib/abslog_web/schema.ex
@@ -1,15 +1,13 @@
 defmodule AbslogWeb.Schema do
-	use Absinthe.Schema
-  import_types AbslogWeb.Schema.ContentTypes
+  use Absinthe.Schema
+  import_types(AbslogWeb.Schema.ContentTypes)
 
   alias AbslogWeb.Resolvers
 
   query do
-
     @desc "Get every post"
     field :posts, list_of(:post) do
-      resolve &Resolvers.Content.list_posts/3
+      resolve(&Resolvers.Content.list_posts/3)
     end
   end
-
 end
